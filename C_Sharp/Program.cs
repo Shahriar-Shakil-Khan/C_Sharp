a1328ce239802bbdf719f1e5200659f68211b67b
Shahriar-Shakil-Khan <shahriarshakil28@gmail.com>
2026-04-13 14:32:06 +0600

    Add project files.

---
+++ C_Sharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(123);
+            Console.WriteLine(3 + 8);
+            Console.ReadLine();
+
+            
+        }
+    }
+}
